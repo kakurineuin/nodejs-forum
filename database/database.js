3bb6cc1b3104ab859e26613b85f1e1f5aaf49e30
Kevin <kakurineuin@gmail.com>
2019-01-04 08:38:04 +0800

    改用 config 套件讀取配置檔。

--- database/database.js
+++ database/database.js
@@ -1,13 +1,12 @@
 const Sequelize = require("sequelize");
-const config =
-  process.env.NODE_ENV === "test"
-    ? require("../config/config_test.json")
-    : require("../config/config.json");
+const config = require("config");
+
+const database = config.get("database");
 
 const sequelize = new Sequelize(
-  config.database.dbname,
-  config.database.user,
-  config.database.password,
+  database.dbname,
+  database.user,
+  database.password,
   {
     dialect: "mysql",
     host: "localhost"
