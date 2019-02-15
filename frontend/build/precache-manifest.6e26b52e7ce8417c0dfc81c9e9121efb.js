812b74a88f062affa31edcffc68e40e860c4fad1
Kevin <kakurineuin@gmail.com>
2019-02-15 23:23:09 +0800

    調整 gitignore。

---
+++ frontend/build/precache-manifest.6e26b52e7ce8417c0dfc81c9e9121efb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "575a2b089d293e5db442",
+    "url": "/static/js/main.575a2b08.chunk.js"
+  },
+  {
+    "revision": "88aa566f39e4126f2c0c",
+    "url": "/static/js/1.88aa566f.chunk.js"
+  },
+  {
+    "revision": "575a2b089d293e5db442",
+    "url": "/static/css/main.74bd7f73.chunk.css"
+  },
+  {
+    "revision": "88aa566f39e4126f2c0c",
+    "url": "/static/css/1.d1873d37.chunk.css"
+  },
+  {
+    "revision": "a63294f1eaaee8128d57134f3c5792e2",
+    "url": "/index.html"
+  }
+];
